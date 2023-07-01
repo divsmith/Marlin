e6cf3057fd91dc4782a16648650baecfbe0a7856
Parker Smith <parker@parkersmith.us>
2023-06-30 23:30:42 -0600

    reduce homing feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2101,7 +2101,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (150*60), (150*60), (10*60) }
+#define HOMING_FEEDRATE_MM_M { (100*60), (100*60), (10*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
